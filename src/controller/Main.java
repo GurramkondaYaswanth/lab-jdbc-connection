a250c1479e960f2dee7a097140651a947b14aebc
GurramkondaYaswanth <gurramkondayaswanth@gmail.com>
2020-09-09 11:59:37 +0530

    ProGrad ID

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -8,6 +8,15 @@ public class Main {
 	public static void main(String args[]) {
 		
 	// Fill your code
+		//establishing connection
+		Connection con  = null;
+		con = ConnectionManager.getConnection();
+		if(con != null) {
+			System.out.println("Connection established");
+		}
+		else {
+			System.out.println("Check your connection");
+		}
 		
 	}
 }
